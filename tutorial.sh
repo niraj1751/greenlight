0b1464c5bac4c0c12fb4d6e9ec635bcf32c05b91
Niraj Raut <1751niraj123@gmail.com>
2026-08-06 16:05:24 +0000

    updated tutorial.sh

---
+++ tutorial.sh
@@ -0,0 +1,22 @@
+rails generate migration AddCustomersTable
+
+###
+
+AddCustomersTable code
+
+###
+
+rails db:migrate
+
+###
+
+now we have to create model
+
+
+
+
+
+#######################################################
+university_schedules - table name
+university_schedule - file name (model)
+UniversitySchedule - classname (modal)
